fe1efb5ab5714b638e7c90d78032e6fc617e279e
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-07 05:17:46 -0800

    index retry

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736254478|481463652';
+const CACHE_VERSION = '1736255808|1811169028';
 /** @type {string} */
 const CACHE_PREFIX = 'jolt-test-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'jolt-test.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["jolt-test.html","jolt-test.js","jolt-test.offline.html","jolt-test.icon.png","jolt-test.apple-touch-icon.png","jolt-test.audio.worklet.js","jolt-test.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHABLE_FILES = ["jolt-test.wasm","jolt-test.pck","jolt-test.side.wasm"];
+const CACHABLE_FILES = ["index.wasm","index.pck","index.side.wasm"];
 const FULL_CACHE = CACHED_FILES.concat(CACHABLE_FILES);
 
 self.addEventListener('install', (event) => {
