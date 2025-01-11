153266efb5dd9d46987175b7acc351888e31f10d
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-11 05:20:13 -0800

    rigidbody character controller added

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736255808|1811169028';
+const CACHE_VERSION = '1736601461|24585928230';
 /** @type {string} */
 const CACHE_PREFIX = 'jolt-test-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
