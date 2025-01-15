26e4017c759c943dffa6e5de7be2d7361448a76f
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-15 05:44:15 -0800

    improved stick controls

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736743635|26076368091';
+const CACHE_VERSION = '1736948596|10463871282';
 /** @type {string} */
 const CACHE_PREFIX = 'jolt-test-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
